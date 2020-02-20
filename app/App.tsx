cf0bfff385632bb453492f5e9f81f728b25ed541
hanhyeonkyu <busgod1212@gmail.com>
2020-02-20 23:08:04 +0900

    postmessage test

--- app/App.tsx
+++ app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -7,43 +7,68 @@ import {
   Text,
   StatusBar,
   Dimensions,
+  Button,
+  Alert,
 } from 'react-native';
 import { WebView } from 'react-native-webview'
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-function App() {
-  const {height, width} = Dimensions.get("window")
-  const [res, setRes] = useState('not arrive a message')
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <View>
-            <WebView 
-              source={{ uri: 'https://03e0e836.ngrok.io' }}
-              style={{ height: height - 200 }}
-            />
+class App extends React.Component {
+
+  public webViewRef: any;
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      res: 'not arrive a message'
+    }
+  }
+
+  onMessage = (event: any) => { this.setState({ res: event.nativeEvent.data }) }
+  reqToWeb = () => { this.webViewRef.postMessage("this message from app"); }
+
+  render() {
+    const { res }: any = this.state
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
             <View>
-              <Text style={styles.resText}>above is web area⬆️ bottom is app area⬇️</Text>
-              <Text style={styles.resText}>{"from web response : " + res}</Text>
+              <WebView
+                ref={(ref) => { this.webViewRef = ref }}
+                source={{ uri: 'https://3f8b744d.ngrok.io' }}
+                originWhitelist={["*"]}
+                style={styles.webview}
+                onMessage={this.onMessage}
+              />
+              <View>
+                <Text style={styles.resText}>above is web area⬆️ bottom is app area⬇️</Text>
+                <Text style={styles.resText}>{"from web response : " + res}</Text>
+                <Button title="Req to Web" onPress={this.reqToWeb} />
+              </View>
             </View>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
+    height: Dimensions.get("window").height,
+    width: Dimensions.get("window").width
   },
   resText: {
     textAlign: "center",
     marginBottom: 5
+  },
+  webview: {
+    height: Dimensions.get("window").height - 200
   }
 });
 
